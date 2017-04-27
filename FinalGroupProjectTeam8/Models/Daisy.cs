ae129854ccc2577399d2d5a510b1e7dd5f294911
Kevbot718 <kstevens7@utexas.edu>
2017-04-27 18:35:52 -0500

    Daisy quick update

--- FinalGroupProjectTeam8/Models/Daisy.cs
+++ FinalGroupProjectTeam8/Models/Daisy.cs
@@ -8,6 +8,6 @@ namespace FinalGroupProjectTeam8.Models
     public class Daisy
     {
         public int Daisyid { get; set; }
-        public String DaisyName { get; set; }
+        public String DaisyName2 { get; set; }
     }
 }
